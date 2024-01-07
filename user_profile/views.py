4806c7a45579337c9e01f20fe7134b78829f7a12
sommy-josh <chisomzzy1@gmail.com>
2024-01-08 00:03:21 +0100

    corrected the permissions

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,13 +3,22 @@ from .serializers import UserProfileSerializer
 from .models import UserProfile
 from rest_framework import generics
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
+
+
+
 
 class UserProfileListCreateView(generics.ListCreateAPIView):
-    authentication_classes=[IsAuthorOrReadOnly]
+
+    permission_classes=(IsAuthorOrReadOnly,)
     queryset=UserProfile.objects.all()
     serializer_class=UserProfileSerializer
 
+    
+
 class UserProfileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes=[IsAuthorOrReadOnly]
+
+    permission_classes=[IsAdminUser]
     queryset=UserProfile.objects.all()
     serializer_class=UserProfileSerializer
