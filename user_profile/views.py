0f520a94b387a48d9993976b3d65129082798aba
sommy-josh <chisomzzy1@gmail.com>
2024-01-07 16:20:27 +0100

    i protected the views

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from .serializers import UserProfileSerializer
 from .models import UserProfile
 from rest_framework import generics
+from .permissions import IsAuthorOrReadOnly
 
 class UserProfileListCreateView(generics.ListCreateAPIView):
+    authentication_classes=[IsAuthorOrReadOnly]
     queryset=UserProfile.objects.all()
     serializer_class=UserProfileSerializer
 
 class UserProfileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes=[IsAuthorOrReadOnly]
     queryset=UserProfile.objects.all()
     serializer_class=UserProfileSerializer
